8d929a556d3e70fb6d342b00b20bdcf858960d8b
Eliav Menachi <eliav.menachi@gmail.com>
2024-02-05 22:50:36 +0200

    update ipost

--- src/services/posts-service.ts
+++ src/services/posts-service.ts
@@ -1,11 +1,11 @@
 import apiClient, { CanceledError } from "./api-client"
 
-import { IPost } from "../Post"
+import { PostData } from "../Post"
 
 export { CanceledError }
 const getAllPosts = () => {
     const abortController = new AbortController()
-    const req = apiClient.get<IPost[]>('studentpost', { signal: abortController.signal })
+    const req = apiClient.get<PostData[]>('studentpost', { signal: abortController.signal })
     return { req, abort: () => abortController.abort() }
 
 }
