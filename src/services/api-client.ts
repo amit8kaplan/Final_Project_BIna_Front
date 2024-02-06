bc186369512ace8f5faa0fffde5e1039f65ddf8a
Eliav Menachi <eliav.menachi@gmail.com>
2024-02-06 12:55:31 +0200

    ip fix

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,7 +2,8 @@ import axios, { CanceledError } from "axios";
 
 export { CanceledError }
 const apiClient = axios.create({
-    baseURL: 'https://10.10.248.100',
+    //baseURL: 'https://10.10.248.100',
+    baseURL: 'https://193.106.55.100',
 });
 
 export default apiClient;
